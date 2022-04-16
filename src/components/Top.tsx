773510ae949a56eecd4e7bb3a032b78991a17b17
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-16 22:49:49 +0900

    add animation of Top component

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,57 +1,27 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-
-const Base = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-const Content = styled.div`
-  height: 40%;
-  min-height: 260px;
-  display: flex;
-  gap: 50px;
-`;
-const ContentBand = styled.div`
-  height: 100%;
-  width: 15px;
-  background-color: tomato;
-`;
-const ContentWrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-const TopMessage = styled.h1`
-  font-size: 64px;
-  line-height: 1.8;
-`;
-const TopMessageBlock = styled.div`
-  display: inline-block;
-  overflow: hidden;
-  height: 80px;
-`;
-const AutherBlock = styled.div`
-  display: inline-block;
-  overflow: hidden;
-  height: 50px;
-`;
-const Auther = styled.p`
-  font-size: 20px;
-`;
+import { Transition, TransitionStatus } from "react-transition-group";
 
 function Top() {
   const [isHover, setIsHover] = useState(false);
-  // const spring = useSpring({
-  //   width: isHover ? "100%" : "15px",
-  //   config: { duration: 250 },
-  // });
+  const transitionStyle = {
+    entering: {
+      transition: "all 1s ease",
+      width: "500px",
+    },
+    entered: {},
+    exiting: {
+      transition: "all 1s ease",
+      width: "15px",
+    },
+    exited: {},
+  };
   return (
     <Base>
-      <Content>
-        <ContentBand className="top-content-band-anime" />
+      <Content onMouseOver={() => setIsHover(true)} onMouseOut={() => setIsHover(false)}>
+        <Transition in={isHover} timeout={1000}>
+          {(state) => <ContentBand state={state} className="top-content-band-anime" />}
+        </Transition>
         <ContentWrapper>
           <TopMessage>
             <TopMessageBlock>
@@ -72,3 +42,47 @@ function Top() {
 }
 
 export default Top;
+
+const Base = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Content = styled.div`
+  max-width: 750px;
+  height: 40%;
+  min-height: 260px;
+  position: relative;
+`;
+const ContentBand = styled.div<{ state: TransitionStatus }>`
+  position: absolute;
+  height: 100%;
+  background-color: tomato;
+  transition: 0.2s;
+  width: ${({ state }) => (state === "entering" || state === "entered" ? "100%" : "15px")};
+`;
+const ContentWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 30px;
+`;
+const TopMessage = styled.h1`
+  font-size: 64px;
+  line-height: 1.8;
+`;
+const TopMessageBlock = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  height: 80px;
+`;
+const AutherBlock = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  height: 50px;
+`;
+const Auther = styled.p`
+  font-size: 20px;
+`;
